3d8757e941b01a0f8faadd9c38bda1356015d619
rodsmade <rodrigogregoldo@gmail.com>
2022-03-03 11:30:27 -0300

    fixed timing of Congratulations! alert

--- main.js
+++ main.js
@@ -118,23 +118,14 @@ function handleSubmitWord() {
 
 	guessedWordCount += 1;
 
-	if (currentWord === allWords[0] && (today_day >= 1 && today_day <= 10)) {
-
-		window.alert("Congratulations!");
-
-	} else if (currentWord === allWords[1] && (today_day >= (13 - 2) && today_day <= (16 + 1))) {
-
-		window.alert("Congratulations!");
-
-	} else if (currentWord === allWords[2] && (today_day >= 18 && today_day <= 24)) {
-
-		window.alert("Congratulations!");
-
-	} else if (currentWord === allWords[3] && (today_day >= 25 && today_day <= 31)) {
-
-		window.alert("Congratulations!");
-
-	}
+	if ((currentWord === allWords[0] && (today_day >= 1 && today_day <= 10))
+	|| (currentWord === allWords[1] && (today_day >= (13 - 2) && today_day <= (16 + 1)))
+	|| (currentWord === allWords[2] && (today_day >= 18 && today_day <= 24))
+	|| (currentWord === allWords[3] && (today_day >= 25 && today_day <= 31))) {
+		setTimeout(() => {
+			window.alert("Congratulations!");
+		}, interval * 6);
+	} 
 
 	if (guessedWords.length > 5) {
 		window.alert(`Sorry, you have no more guesses. The word is ${wordOfTheDay}.`)
